4fd4178ed3c746f6eafd5215b68c98781b371881
TREE <apichai.samuttong@gmail.com>
2022-03-02 23:36:56 +0700

    GameTest

---
+++ NOBOIShooter/Controls/GameArea.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace NOBOIShooter.Controls
+{
+    public class GameArea : Component
+    {
+        #region Fields
+
+        private MouseState _currentMouse;
+
+        private SpriteFont _font;
+
+        private bool _isHovering;
+
+        private MouseState _previousMouse;
+
+        private Rectangle mouseRectangle;
+
+        private Color background;
+
+        private int posX;
+
+        private int posY;
+
+        private int width;
+
+        private int height;
+
+        Texture2D whiteRectangle;
+
+        #endregion
+
+        #region Properties
+
+        public event EventHandler Click;
+
+        public bool Clicked { get; private set; }
+
+        public Vector2 Position { get; set; }
+
+
+        #endregion
+
+        #region Methods
+
+        public GameArea(GraphicsDevice graphicsDevice, int x, int y, int Width,int Height)
+        {
+            posX = x;
+            posY = y;
+            Position = new Vector2(x,y);
+            width = Width;
+            height = Height;
+
+            whiteRectangle = new Texture2D(graphicsDevice, 1, 1);
+            whiteRectangle.SetData(new[] { Color.White });
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(whiteRectangle, new Rectangle(posX, posY,width, height), Color.Chocolate);
+        }
+
+
+
+        public override void Update(GameTime gameTime)
+        {
+            
+        }
+
+        #endregion
+    }
+}
--- NOBOIShooter/States/GameState.cs
+++ NOBOIShooter/States/GameState.cs
@@ -4,6 +4,7 @@ using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using NOBOIShooter.Controls;
 
 namespace NOBOIShooter.States
 {
@@ -11,18 +12,24 @@ namespace NOBOIShooter.States
     public class GameState : State
     {
         private SpriteFont myText;
+        Texture2D whiteRectangle;
+        private GameArea gameArea;
 
         public GameState(Main game, GraphicsDevice graphicsDevice, ContentManager content)
             : base(game, graphicsDevice, content)
         {
             myText = _content.Load<SpriteFont>("Fonts/Font");
+            whiteRectangle = new Texture2D(graphicsDevice, 1, 1);
+            whiteRectangle.SetData(new[] { Color.White });
+            gameArea = new GameArea(graphicsDevice,30, 30, 800, 650);
         }
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
             spriteBatch.Begin();
-
-            spriteBatch.DrawString(myText, "Can u see me? \n sorry It's too white!", new Vector2(300,300), Color.Black);
+            gameArea.Draw(gameTime, spriteBatch);
+            spriteBatch.Draw(whiteRectangle, new Rectangle(40, 40, 40, 40), Color.Pink);
+            spriteBatch.DrawString(myText, "Can u see me? \n sorry It's too white!", new Vector2(900, 100), Color.Black);
             spriteBatch.End();
         }
 
@@ -35,5 +42,7 @@ namespace NOBOIShooter.States
         {
 
         }
+
+
     }
 }
