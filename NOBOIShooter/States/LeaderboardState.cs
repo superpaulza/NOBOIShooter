29fa367c2a1f45bba12707d804c3a37be120eae7
lebrancconvas <lebrancconvas@gmail.com>
2022-03-05 05:06:29 +0700

    :truck: Add New Class File.

---
+++ NOBOIShooter/States/LeaderboardState.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+using NOBOIShooter.Controls;
+using NOBOIShooter.GameObjects;
+using System;
+
+namespace NOBOIShooter.States
+{
+    public class LeaderboardState : State
+    {
+        public LeaderboardState(Main game, GraphicsDevice graphicsDevice, ContentManager content)
+            : base(game, graphicsDevice, content)
+        {
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void PostUpdate(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
