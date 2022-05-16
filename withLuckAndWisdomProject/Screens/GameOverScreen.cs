43d7c9c961bfd003e07bf75b7f8f7f7f209198d8
lebrancconvas <lebrancconvas@gmail.com>
2022-05-16 20:23:21 +0700

    :sparkles: Create GameOverScreen Class.

---
+++ withLuckAndWisdomProject/Screens/GameOverScreen.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace withLuckAndWisdomProject.Screens
+{
+    public class GameOverScreen
+    {
+        public GameOverScreen()
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
