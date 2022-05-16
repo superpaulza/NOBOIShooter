2013f192363347c2dfd924dedee44e5576967b23
lebrancconvas <lebrancconvas@gmail.com>
2022-05-16 19:38:49 +0700

    :sparkles: Create HUD Class.

---
+++ withLuckAndWisdomProject/Screens/HUD.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+namespace withLuckAndWisdomProject.Screens
+{
+    public class HUD : GameScreen
+    {
+        public HUD()
+        {
+
+        }
+    }
+}
