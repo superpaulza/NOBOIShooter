b5741c8aaf33e74a1646c232981b886d20e5bfca
Treast <apichai.samuttong@gmail.com>
2022-05-21 22:17:05 +0700

    Bamboo leaf

--- withLuckAndWisdomProject/Object/Bamboo.cs
+++ withLuckAndWisdomProject/Object/Bamboo.cs
@@ -38,10 +38,12 @@ namespace withLuckAndWisdomProject.Object
         private BambooState _state;
         private Texture2D _joint1texture;
         private Texture2D _joint2texture;
+        private Texture2D _leaftexture;
         private float _height;
         private float _width;
 
         private List<Point> _joint;
+        private List<float> _leaf;
         private int _jointHeight;
         private Vector2 _pandaPosition;
 
@@ -55,7 +57,8 @@ namespace withLuckAndWisdomProject.Object
 
             _joint1texture = ResourceManager.BambooJoint1;
             _joint2texture = ResourceManager.BambooJoint2;
-            
+            _leaftexture = ResourceManager.BambooLeaf;
+
             _jointHeight = SET_HEIGHT_OF_JOINT;
             _width = SET_WIDTH_OF_BAMBOOS;
             
@@ -65,7 +68,15 @@ namespace withLuckAndWisdomProject.Object
             for (int i = 0; i < height ; i+= _jointHeight)
                 _joint.Add(new Point(0, i));
 
-            if(bambooType == BambooType.Pandle)
+            Random random = new Random();
+            _leaf = new List<float>();
+            for (int i = 0; i < _joint.Count; i ++)
+            {
+                _leaf.Add( MathHelper.ToRadians(i % 2 == 0 ? random.Next(-60, -30) : random.Next(30,60) ) );
+
+            }
+
+            if (bambooType == BambooType.Pandle)
             {
                 _pandaPosition = new Vector2(-_width /2  -ResourceManager.Panda.Width, -height/2);
             }
@@ -99,16 +110,23 @@ namespace withLuckAndWisdomProject.Object
             //spriteBatch.Draw(ResourceManager.Pencil, new Rectangle((int)_body.Position.X, (int)_body.Position.Y, (int)_width, (int)_height), null,
             //    Color.Lime, _body.Rotation, new Vector2(.5f, .5f), SpriteEffects.None, 0);
 
+            if(_type != BambooType.Friable)
+                for (int i = 0; i < _leaf.Count; i++)
+                    spriteBatch.Draw(_leaftexture, new Rectangle((int)Body.Position.X + _joint[i].X, (int)(Body.Position.Y - _height / 2 + _joint[i].Y), (int)_width, (int)_jointHeight), null,
+                          Color.LightGreen, _leaf[i], new Vector2(0, _leaftexture.Height), SpriteEffects.None, 0);
+
             if (_type == BambooType.Normal)
             {
                 // Draw basic bamboo
                 spriteBatch.Draw(ResourceManager.BambooShoot, new Rectangle((int)Body.Position.X , (int)(Body.Position.Y - _height / 2 - _jointHeight / 2), (int)_width, (int)_jointHeight), null,
                          Color.LightGreen, Body.Rotation, new Vector2(_joint1texture.Width / 2, _joint1texture.Height / 2), SpriteEffects.None, 0);
+                
+
                 for (int i = 0; i < _joint.Count; i++)
-                {
+               
                     spriteBatch.Draw(i % 2 == 0 ? _joint1texture : _joint2texture, new Rectangle((int)Body.Position.X + _joint[i].X, (int)(Body.Position.Y - _height/2 + _jointHeight/2  + _joint[i].Y), (int)_width, (int)_jointHeight), null,
                          i < 2 ? Color.LightGreen : Color.Green, Body.Rotation, new Vector2(_joint1texture.Width / 2, _joint1texture.Height / 2), SpriteEffects.None, 0);
-                }
+               
             }
 
             if (_type == BambooType.Friable)
