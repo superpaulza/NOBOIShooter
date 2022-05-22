ed136914351b2f06c5ec94d68557ddcdba0c666c
superpaulza <18383834+superpaulza@users.noreply.github.com>
2022-05-22 18:40:51 +0700

    fix bug after merge

--- withLuckAndWisdomProject/Data/ShareData.cs
+++ withLuckAndWisdomProject/Data/ShareData.cs
@@ -5,7 +5,6 @@ using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
-using withLuckAndWisdomProject.Manager;
 
 namespace withLuckAndWisdomProject.Data
 {
